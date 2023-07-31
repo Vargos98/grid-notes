d0ca2b6658fbd044fd9c7096af28bbf202a7071a
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-07-31 10:55:10 +0530

    Note page design completed

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,5 +1,8 @@
 import {CiSearch} from 'react-icons/ci'
-
+import dummyNotes from '../dummy_notes'
+import { Link } from 'react-router-dom'
+import {BsPlusLg} from 'react-icons/bs'
+import NoteItem from '../components/NoteItem'
 const Notes = () => {
   return (
     <section>
@@ -9,8 +12,13 @@ const Notes = () => {
             <button className='btn'><CiSearch /></button>
         </header>
         <div className="notes__container">
-            
+            {
+                dummyNotes.map(note => <NoteItem key={note.id} note= {note}/>)
+            }
+
         </div>
+
+        <Link className='btn add__btn'><BsPlusLg/></Link>
     </section>
   )
 }
