dd8cf205764267327e8bc6f281ce6533dc474963
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-07-31 16:53:00 +0530

    Solved the useEffect error
    Solved the useEffect error. Added x icon in search , also added a msg if no notes are found in search

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,6 +1,7 @@
 import {CiSearch} from 'react-icons/ci'
-import { useEffect, useState } from 'react'
+import { useState , useEffect} from 'react' 
 import { Link } from 'react-router-dom'
+import {MdClose} from 'react-icons/md'
 import {BsPlusLg} from 'react-icons/bs'
 import NoteItem from '../components/NoteItem'
 const Notes = ({notes}) => {
@@ -12,13 +13,13 @@ const Notes = ({notes}) => {
   const handleSearch = () =>{
 
       setFilteredNotes(notes.filter(note => {
-        if(note.title.toLowerCase().match(text.toLocaleLowerCase)){
+        if(note.title.toLowerCase().match(text.toLocaleLowerCase())){
           return note;
         }
       }))
-
+      
   }
-  // useEffect(handleSearch, [text]);
+  useEffect(handleSearch, [text]);
   
   
   return (
@@ -27,9 +28,10 @@ const Notes = ({notes}) => {
             {!showSearch && <h2>Grid Notes</h2>}
             {showSearch && <input type='text' value={text} onChange={(e) => {setText(e.target.value); 
             handleSearch();}} autoFocus placeholder='Keyword...'/>}
-            <button className='btn'onClick={()=> setShowSearch(prevState => !prevState)}><CiSearch /></button>
+            <button className='btn'onClick={()=> setShowSearch(prevState => !prevState)}>{showSearch ? <MdClose /> : <CiSearch />}</button>
         </header>
         <div className="notes__container">
+              {filteredNotes.length == 0 &&<p className='empty__notes'>No Notes Found !</p>}
             {
                 filteredNotes.map(note => <NoteItem key={note.id} note= {note}/>)
             }
